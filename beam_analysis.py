2323080eb3946b44452bc88e9f305cd18ab647e4
Charlie Boardman <cboardman2@gmail.com>
2022-07-28 20:43:29 -0700

    Added 2x10 beams

--- beam_analysis.py
+++ beam_analysis.py
@@ -64,5 +64,6 @@ def max_normal_stress_uniform_load(beam,w,span):
 b2x4 = beam(5.359375,1.5*3.5,3.5,'wood')
 b2x6 = beam(20.796875,1.5*5.5,5.5,'wood')
 b2x8 = beam(47.634765625,1.5*7.25,7.25,'wood')
+b2x10 = beam(98.9316400625,1.5*9.25,9.25,'wood')
 b2x12 = beam(177.978515625,1.5*11.25,11.25,'wood')
 c4x7_25 = beam(4.59,2.13,4,'steel')
